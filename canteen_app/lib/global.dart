63192c48e4ae322a35efe3f58bd122a5e074978e
Sweta Kyada <swetakyada20@gmail.com>
2020-11-21 15:00:00 +0530

    ui modified

--- canteen_app/lib/global.dart
+++ canteen_app/lib/global.dart
@@ -7,9 +7,9 @@ var prices = new List<int>();
 var uid;
 int total = calculateTotal();
 
-int calculateTotal(){
+int calculateTotal() {
   int total = 0;
-  for (var i = 0; i < items.length; i++){
+  for (var i = 0; i < items.length; i++) {
     total += quantity[i] * prices[i];
   }
   print(total);
@@ -25,85 +25,151 @@ class MyApp extends StatelessWidget {
       home: new ListDisplay(),
     );
   }
-}class ListDisplay extends StatelessWidget {
+}
+
+class ListDisplay extends StatelessWidget {
   @override
-  Widget build (BuildContext ctxt) {
+  Widget build(BuildContext ctxt) {
     return new Scaffold(
-        body: ListView.builder(
-                            itemCount: items.length,
-                            itemBuilder: (BuildContext ctxt, int index) {
-                                      return Container(
-                                        alignment: Alignment.center,
-                                        width: double.infinity,
-                                        height: 70,
-                                        decoration: BoxDecoration(boxShadow: [
-                                          BoxShadow(
-                                            color: Color(0xFFfae3e2).withOpacity(0.1),
-                                            spreadRadius: 1,
-                                            blurRadius: 1,
-                                            offset: Offset(0, 1),
-                                          ),
-                                        ]),
-                                        child: Card(
-                                          color: Colors.white,
-                                          elevation: 0,
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius: const BorderRadius.all(
-                                              Radius.circular(5.0),
-                                            ),
-                                          ),
-                                          child: Container(
-                                            alignment: Alignment.center,
-                                            padding: EdgeInsets.only(left: 25, right: 30, top: 10, bottom: 10),
-                                            child: Column(
-                                              children: <Widget>[
-                                                SizedBox(
-                                                  height: 15,
-                                                ),
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                  children: <Widget>[
-                                                    Text(
-                                                      items[index],
-                                                      style: TextStyle(
-                                                          fontSize: 18,
-                                                          color: Color(0xFF3a3a3b),
-                                                          fontWeight: FontWeight.w600),
-                                                      textAlign: TextAlign.left,
-                                                    ),
-                                                    Text(
-                                                        (prices[index]).toString(),
-                                                      style: TextStyle(
-                                                          fontSize: 18,
-                                                          color: Color(0xFF3a3a3b),
-                                                          fontWeight: FontWeight.w600),
-                                                      textAlign: TextAlign.left,
-                                                    ),
-                                                    Text(
-                                                      (quantity[index]).toString(),
-                                                      style: TextStyle(
-                                                          fontSize: 18,
-                                                          color: Color(0xFF3a3a3b),
-                                                          fontWeight: FontWeight.w600),
-                                                      textAlign: TextAlign.left,
-                                                    ),
-                                                    Text(
-                                                      (prices[index] * quantity[index]).toString(),
-                                                      style: TextStyle(
-                                                          fontSize: 18,
-                                                          color: Color(0xFF3a3a3b),
-                                                          fontWeight: FontWeight.w600),
-                                                      textAlign: TextAlign.left,
-                                                    )
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                     }
-    ),
-        );
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 70,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Color(0xFFfae3e2).withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: Offset(0, 1),
+                ),
+              ]),
+              child: Card(
+                color: Colors.white,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding:
+                      EdgeInsets.only(left: 25, right: 30, top: 10, bottom: 10),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                width: 140,
+                                child: Text(
+                                  items[index],
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF3a3a3b),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 60,
+                                child: Text(
+                                  (prices[index]).toString(),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF3a3a3b),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 30,
+                                child: Text(
+                                  (quantity[index]).toString(),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF3a3a3b),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 40,
+                                child: Text(
+                                  (prices[index] * quantity[index]).toString(),
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF3a3a3b),
+                                  ),
+                                  textAlign: TextAlign.right,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      /*Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            items[index],
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF3a3a3b),
+                                fontWeight: FontWeight.w600),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            (prices[index]).toString(),
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF3a3a3b),
+                                fontWeight: FontWeight.w600),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            (quantity[index]).toString(),
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF3a3a3b),
+                                fontWeight: FontWeight.w600),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            (prices[index] * quantity[index]).toString(),
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF3a3a3b),
+                                fontWeight: FontWeight.w600),
+                            textAlign: TextAlign.left,
+                          )
+                        ],
+                      ),*/
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
--- canteen_app/lib/pages/SignInPage.dart
+++ canteen_app/lib/pages/SignInPage.dart
@@ -17,169 +17,173 @@ class _SignInPageState extends State<SignInPage> {
     double defaultFontSize = 14;
     double defaultIconSize = 17;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white70,
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: InkWell(
-                child: Container(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Icon(Icons.close),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white70,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: InkWell(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Icon(Icons.close),
+                    ),
                   ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
               ),
-            ),
-            Flexible(
-              flex: 8,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 230,
-                    height: 100,
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      "assets/images/menus/ic_food_express.png",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    showCursor: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
-                      ),
-                      filled: true,
-                      prefixIcon: Icon(
-                        Icons.phone,
-                        color: Color(0xFF666666),
-                        size: defaultIconSize,
-                      ),
-                      fillColor: Color(0xFFF2F3F5),
-                      hintStyle: TextStyle(
-                          color: Color(0xFF666666),
-                          fontFamily: defaultFontFamily,
-                          fontSize: defaultFontSize),
-                      hintText: "Phone Number",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    showCursor: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
-                      ),
-                      filled: true,
-                      prefixIcon: Icon(
-                        Icons.lock_outline,
-                        color: Color(0xFF666666),
-                        size: defaultIconSize,
-                      ),
-                      suffixIcon: Icon(
-                        Icons.remove_red_eye,
-                        color: Color(0xFF666666),
-                        size: defaultIconSize,
-                      ),
-                      fillColor: Color(0xFFF2F3F5),
-                      hintStyle: TextStyle(
-                        color: Color(0xFF666666),
-                        fontFamily: defaultFontFamily,
-                        fontSize: defaultFontSize,
-                      ),
-                      hintText: "Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    child: Text(
-                      "Forgot your password?",
-                      style: TextStyle(
-                        color: Color(0xFF666666),
-                        fontFamily: defaultFontFamily,
-                        fontSize: defaultFontSize,
-                        fontStyle: FontStyle.normal,
-                      ),
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  SignInButtonWidget(),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  FacebookGoogleLogin()
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              Flexible(
+                flex: 8,
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
+                      width: 230,
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        "assets/images/menus/ic_food_express.png",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      showCursor: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        fillColor: Color(0xFFF2F3F5),
+                        hintStyle: TextStyle(
+                            color: Color(0xFF666666),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize),
+                        hintText: "Phone Number",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      showCursor: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.remove_red_eye,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        fillColor: Color(0xFFF2F3F5),
+                        hintStyle: TextStyle(
+                          color: Color(0xFF666666),
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize,
+                        ),
+                        hintText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      width: double.infinity,
                       child: Text(
-                        "Don't have an account? ",
+                        "Forgot your password?",
                         style: TextStyle(
                           color: Color(0xFF666666),
                           fontFamily: defaultFontFamily,
                           fontSize: defaultFontSize,
                           fontStyle: FontStyle.normal,
                         ),
+                        textAlign: TextAlign.end,
                       ),
                     ),
-                    InkWell(
-                      onTap: () => {
-                        Navigator.push(context, ScaleRoute(page: SignUpPage()))
-                      },
-                      child: Container(
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SignInButtonWidget(),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    FacebookGoogleLogin()
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
                         child: Text(
-                          "Sign Up",
+                          "Don't have an account? ",
                           style: TextStyle(
-                            color: Color(0xFFf7418c),
+                            color: Color(0xFF666666),
                             fontFamily: defaultFontFamily,
                             fontSize: defaultFontSize,
                             fontStyle: FontStyle.normal,
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      InkWell(
+                        onTap: () => {
+                          Navigator.push(
+                              context, ScaleRoute(page: SignUpPage()))
+                        },
+                        child: Container(
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Color(0xFFf7418c),
+                              fontFamily: defaultFontFamily,
+                              fontSize: defaultFontSize,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
