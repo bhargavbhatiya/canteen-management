796eb6f5e230f67228fb1b36ca3fd9e86803289d
Manzee1609 <mansikalaria16@gmail.com>
2020-11-21 17:59:59 +0530

    fixed some bugs

--- canteen_app/lib/pages/SignUpPage.dart
+++ canteen_app/lib/pages/SignUpPage.dart
@@ -267,10 +267,15 @@ class SignUpPage extends StatelessWidget {
                       height: 15,
                     ),
                     SignInButtonWidget(),
-                    /*SizedBox(
+                    SizedBox(
                       height: 10,
-                    ),*/
-                    //FacebookGoogleLogin()
+                    ),
+                    Text("or"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SignInButtonWidget2(),
+                    // FacebookGoogleLogin()
                   ],
                 ),
               ),
@@ -324,8 +329,53 @@ class SignUpPage extends StatelessWidget {
     );
   }
 }
-
 class SignInButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Color(0xFFfbab66),
+          ),
+          BoxShadow(
+            color: Color(0xFFf7418c),
+          ),
+        ],
+        gradient: new LinearGradient(
+            colors: [Color(0xFFf7418c), Color(0xFFfbab66)],
+            begin: const FractionalOffset(0.2, 0.2),
+            end: const FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: MaterialButton(
+          highlightColor: Colors.transparent,
+          splashColor: Color(0xFFf7418c),
+          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          child: Padding(
+            padding:
+            const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+            child: Text(
+              "SIGN UP",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontFamily: "WorkSansBold"),
+            ),
+          ),
+// /<<<<<<< ui
+          onPressed: () => {createRecord(context)}),
+// =======
+      // onPressed: () => {createRecord()}),
+// />>>>>>> main
+    );
+  }
+}
+
+class SignInButtonWidget2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -355,10 +405,10 @@ class SignInButtonWidget extends StatelessWidget {
             padding:
                 const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
             child: Text(
-              "SIGN UP",
+              "SIGN UP WITH GOOGLE",
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 25.0,
+                  fontSize: 20.0,
                   fontFamily: "WorkSansBold"),
             ),
           ),
@@ -376,6 +426,8 @@ class SignInButtonWidget extends StatelessWidget {
     );
   }
 }
+
+
 
 class FacebookGoogleLogin extends StatelessWidget {
   @override
