3e538467097c3322636ca971e0e975f30b5fb841
bhargavbhatiya <bhatiyabhargav.bb@gmail.com>
2020-11-22 08:41:21 +0530

    solved conflicts

--- admin_canteen/lib/show_orders.dart
+++ admin_canteen/lib/show_orders.dart
@@ -93,7 +93,6 @@ class VideoScreenState extends State<ShowOrders> {
                         ),
                         Padding(
                             padding: const EdgeInsets.only(left: 15),
-<<<<<<< HEAD
                             child: Column(children: [
                               for (int i = 0; i < items.length; i++)
                                 Text(items[i] +
@@ -141,47 +140,6 @@ class VideoScreenState extends State<ShowOrders> {
                                       })
                                 ],
                               ),
-=======
-                            child: Row(children: [
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.payments,
-                                    color: Colors.green,
-                                  ),
-                                  onPressed: () {
-                                    _paymentTrue(document.id);
-                                  }),
-                              IconButton(
-                                  icon: Icon(Icons.payments_outlined),
-                                  color: Colors.red,
-                                  onPressed: () {
-                                    _paymentFalse(document.id);
-                                  }),
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.circle,
-                                    color: Colors.black12,
-                                  ),
-                                  onPressed: () {
-                                    _changeStatusToA(document.id);
-                                  }),
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.circle,
-                                    color: Colors.red,
-                                  ),
-                                  onPressed: () {
-                                    _changeStatusToB(document.id);
-                                  }),
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.circle,
-                                    color: Colors.green,
-                                  ),
-                                  onPressed: () {
-                                    _changeStatusToC(document.id);
-                                  })
->>>>>>> f43c3ddacbc4d4a5aae7ae62491917f761f44419
                             ])),
                       ])),
                 );
