e73a2d1af906ed9477eb65ca7a91669d2bf47230
Manzee1609 <mansikalaria16@gmail.com>
2020-11-22 09:59:28 +0530

    added canteen open close function

--- canteen_app/lib/main.dart
+++ canteen_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:canteen_app/pages/SignUpPage.dart';
 import 'package:canteen_app/pages/FoodOrderPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 //void main() async => runApp(MaterialApp(
 //    debugShowCheckedModeBanner: false,
 //  theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
@@ -10,7 +11,43 @@ import 'package:canteen_app/pages/FoodOrderPage.dart';
 //));
 
 void main() async {
+  var state = "true";
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(SignUpPage());
-}
+  FirebaseFirestore.instance.collection("startstop").document("ZR6zBFDKChGnlf0WRe7W").get().then((value) {
+    state = value.data()['value'].toString();
+    print(state);
+    if (state == "true"){
+      runApp(SignUpPage());}
+    else{
+      runApp(closed());}
+  });
+    /*if (state == "true")
+    runApp(SignUpPage());
+    else
+      runApp(closed());*/
+  }
+
+
+  class closed extends StatefulWidget {
+    @override
+    _closedState createState() => _closedState();
+  }
+
+  class _closedState extends State<closed> {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("The canteen is closed"),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+  }
